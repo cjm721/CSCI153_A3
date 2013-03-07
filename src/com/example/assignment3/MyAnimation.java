9a55b1600ac64d191c5b10f7b30081440367620f
cjm721 <cjmiller721@gmail.com>
2013-03-07 15:07:46 -0600

    Initial

---
+++ src/com/example/assignment3/MyAnimation.java
@@ -0,0 +1,26 @@
+package com.example.assignment3;
+
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+
+public class MyAnimation implements AnimationListener{
+
+	@Override
+	public void onAnimationEnd(Animation an) {
+		findViewByID(R.id.bTopLeft);
+		
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation an) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAnimationStart(Animation an) {
+		// TODO Auto-generated method stub
+		
+	}
+	public 
+}
